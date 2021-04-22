65291e5b172cc6d6e1ffbfe6ef7eda76c1a326a9
alexo <alexander.omahoney@dvla.gov.uk>
2021-04-22 16:05:21 +0100

    QEB-486 Add additional unit tests and provide yard.doc for the gem

--- lib/symbolize.rb
+++ lib/symbolize.rb
@@ -6,14 +6,26 @@ require_relative 'symbolize/translations.rb'
 module Symbolize
   class Error < StandardError; end
 
+  # Returns the translations object, initializing it if necessary.
+  #
+  # @return [Translations] the translations object.
   def self.translations
     @translations ||= Translations.new
   end
 
-  def self.translate
+  # Configures the Symbolize environment.
+  #
+  # @yieldparam [Translations] config the translations object yielded to the block.
+  def self.translate(&block)
     yield translations
   end
 
+  # Symbolizes a String object.
+  #
+  # @param str [String] the String object to be symbolized.
+  #
+  # @example Symbolize a string using the symbolize method
+  #   symbolize("hello world!") #=> :hello_world
   def self.symbolize(str)
     return str if str.is_a?(Symbol) || str.nil?
     str.downcase.tr(translations.underscore.join, '_')&.tr(translations.remove.join, '')&.to_sym
--- lib/symbolize/translations.rb
+++ lib/symbolize/translations.rb
@@ -1,21 +1,48 @@
 module Symbolize
+  # The translations class holds the attributes used to transform a String object.
+  # It also provides helper methods to manipulate those attributes.
   class Translations
 
-    attr_accessor :underscore, :remove
+    # @return [Array] the characters to be transformed into underscores.
+    attr_accessor :underscore
+    # @return [Array] the characters to be removed from the String.
+    attr_accessor :remove
 
+
+    # Creates an instance of the Translations class.
+    #
+    # Sets the class variables to a default state.
     def initialize
       @underscore = [' ']
       @remove = ['\'','(',')']
     end
 
+    # Merges the String passed with the @underscore Array omitting duplicates.
+    # Removes those characters from the @remove Array to avoid the change being over-written.
+    #
+    # @param t [String] a String object containing characters to be underscored.
+    #
+    # @return [Array] the Array of characters to be underscored.
+    #
+    # @raise [ArgumentError] if the param does not respond to  #to_s.
     def to_underscore(t)
-      @underscore |= t.chars
-      @remove -= t.chars
+      raise ArgumentError "needs to be a String or respond to #to_s" unless t.respond_to?(:to_s)
+      @remove -= t.to_s.chars
+      @underscore |= t.to_s.chars
     end
 
+    # Merges the String passed with the @remove Array omitting duplicates.
+    # Removes those characters from the @underscore Array to avoid the change being over-written.
+    #
+    # @param t [String] a String object containing characters to be removed.
+    #
+    # @return [Array] the Array of characters to be removed.
+    #
+    # @raise [ArgumentError] if the param does not respond to  #to_s.
     def to_remove(t)
-      @remove |= t.chars
-      @underscore -= t.chars
+      raise ArgumentError "needs to be a String or respond to #to_s" unless t.respond_to?(:to_s)
+      @underscore -= t.to_s.chars
+      @remove |= t.to_s.chars
     end
   end
 end
